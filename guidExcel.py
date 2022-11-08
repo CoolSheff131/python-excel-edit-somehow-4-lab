a1ee07dcfb552c4b8e59a0e5713f182b05623888
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-11-08 15:35:12 +0800

    read and write sheets

--- guidExcel.py
+++ guidExcel.py
@@ -9,7 +9,10 @@ from design import Ui_Dialog
 from PyQt5.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox, QTableWidgetItem
 )
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 import pandas as pd  # pip install pandas
 import numpy as np
 from xlrd import open_workbook
@@ -29,7 +32,6 @@ class Window(QMainWindow, Ui_Dialog):
         self.loadHappyBtn.clicked.connect(self.loadHappyData)
         self.saveRazdachaBtn.clicked.connect(self.saveRazdacha)
 
-        self.table.cellClicked.connect(self.cellClicked)
 
         self.path1 = './self.path1.xlsx'
 
@@ -37,6 +39,7 @@ class Window(QMainWindow, Ui_Dialog):
 
     ### По нажатию на ячейку в таблице раздачи, взять все строки в которых есть имя покупателя из happy.
     def cellClicked(self, row, column):
+        table = self.tabWidget.currentWidget()
         while (self.table_3.rowCount() > 0):
             self.table_3.removeRow(0)
 
@@ -45,7 +48,7 @@ class Window(QMainWindow, Ui_Dialog):
         self.table_3.setRowCount(rowCount)
         self.table_3.setColumnCount(columnCount)
 
-        clickedRowText = self.table.item(row, 1).text()
+        clickedRowText = table.item(row, 1).text()
         print(clickedRowText)
         self.label_3.setText(clickedRowText)
         lastRowInsertedIndex = 0
@@ -80,41 +83,45 @@ class Window(QMainWindow, Ui_Dialog):
 
     def saveRazdacha(self):
         filename = '2self.path1.xlsx'
-        table = self.table
         print('Save razdacha')
         wb = Workbook()
-        sheet = wb.active
-        for row in range(table.rowCount()):
-            for column in range(table.columnCount()):
-                text = self.table.item(row, column).text()
+        wb.remove(wb.active)
+        for tabIndex in range (self.tabWidget.count()):
+            print(self.tabWidget.tabText(tabIndex))
+            sheet = wb.create_sheet(str(self.tabWidget.tabText(tabIndex)))
 
-                try:
-                    print(row, column)
-                    text = str(table.item(row, column).text())
-                    print(text)
-                    sheet.cell(row+1, column+1).value = text
+            table = self.tabWidget.widget(tabIndex)
 
-                    background = table.item(row, column).background()
-                    redFill = PatternFill(start_color='FFFFFFFF',
-                                          end_color='FFFFFFFF',
-                                          fill_type='solid')
 
-                    if (background == Qt.yellow):
-                        redFill = PatternFill(start_color='FFFFFF00',
-                                              end_color='FFFFFF00',
-                                              fill_type='solid')
-                    elif (background == Qt.red):
-                        redFill = PatternFill(start_color='FFFF0000',
-                                              end_color='FFFF0000',
-                                              fill_type='solid')
-                    elif (background == Qt.green):
-                        redFill = PatternFill(start_color='FF00ff00',
-                                              end_color='FF00ff00',
+            for row in range(table.rowCount()):
+                for column in range(table.columnCount()):
+                    text = table.item(row, column).text()
+
+                    try:
+                        text = str(table.item(row, column).text())
+                        sheet.cell(row+1, column+1).value = text
+
+                        background = table.item(row, column).background()
+                        redFill = PatternFill(start_color='FFFFFFFF',
+                                              end_color='FFFFFFFF',
                                               fill_type='solid')
 
-                    sheet.cell(row+1, column+1).fill = redFill
-                except AttributeError:
-                    pass
+                        if (background == Qt.yellow):
+                            redFill = PatternFill(start_color='FFFFFF00',
+                                                  end_color='FFFFFF00',
+                                                  fill_type='solid')
+                        elif (background == Qt.red):
+                            redFill = PatternFill(start_color='FFFF0000',
+                                                  end_color='FFFF0000',
+                                                  fill_type='solid')
+                        elif (background == Qt.green):
+                            redFill = PatternFill(start_color='FF00ff00',
+                                                  end_color='FF00ff00',
+                                                  fill_type='solid')
+
+                        sheet.cell(row+1, column+1).fill = redFill
+                    except AttributeError:
+                        pass
 
         wb.save(filename)
 
@@ -122,28 +129,42 @@ class Window(QMainWindow, Ui_Dialog):
     def loadRazdacha(self):
         while (self.table_3.rowCount() > 0):
             self.table_3.removeRow(0)
+
+
         sheetName = 'дост26.12 (2)'
 
-        wb_obj = openpyxl.load_workbook(self.path1)
+        wb_obj = openpyxl.load_workbook(self.path1, data_only=True)
+
+        self.tabWidget.clear()
+        for sheetItemName in wb_obj.sheetnames:
+            tab = QTabWidget()
+            table = QTableWidget()
+            self.tabWidget.addTab(table, sheetItemName)
+            sheet_obj = wb_obj[sheetItemName]
+            m_row = sheet_obj.max_row
+            m_column = sheet_obj.max_row
+            table.cellClicked.connect(self.cellClicked)
+
+            table.setRowCount(m_row)
+            table.setColumnCount(m_column)
+            for i in range(1, m_row + 1):
+                for j in range(1, m_column + 1):
+                    cell_obj = sheet_obj.cell(row=i, column=j)
+                    tableItem = QTableWidgetItem(str(cell_obj.value or ''))
+                    table.setItem(i - 1, j - 1, tableItem)
+                    table
+                    colorHex = cell_obj.fill.start_color.index
+                    if (colorHex == 'FFFFFF00'):
+                        tableItem.setBackground(Qt.yellow);
+                    elif (colorHex == 7):
+                        tableItem.setBackground(Qt.red);
+                    elif (colorHex == 'FF92D050'):
+                        tableItem.setBackground(Qt.green);
+            table.setColumnWidth(2, 300)
+
+            # tab.children().append(table)
         self.wb1 = wb_obj
-        sheet_obj = wb_obj[sheetName]
-        m_row = sheet_obj.max_row
-        m_column = sheet_obj.max_row
-        self.table.setRowCount(m_row)
-        self.table.setColumnCount(m_column)
-        for i in range(1, m_row + 1):
-            for j in range(1, m_column +1 ):
-                cell_obj = sheet_obj.cell(row=i, column=j)
-                tableItem = QTableWidgetItem(str(cell_obj.value or ''))
-                self.table.setItem(i -1, j -1, tableItem)
-                colorHex = cell_obj.fill.start_color.index
-                if (colorHex == 'FFFFFF00'):
-                    tableItem.setBackground(Qt.yellow);
-                elif (colorHex == 7): 
-                    tableItem.setBackground(Qt.red);
-                elif (colorHex == 'FF92D050'):
-                    tableItem.setBackground(Qt.green);
-        self.table.setColumnWidth(2, 300)
+
 
 
     ### Загруить happy.xlsx. Все страницы в таблицу
